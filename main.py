143e50500bdbb4eb03b89e946135b27ef41c6297
c0nn3r <connervercellino@gmail.com>
2017-07-13 16:18:41 -0700

    First implementation of batch sorting

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ import torch.backends.cudnn as cudnn
 
 import torchvision
 import torchvision.transforms as transforms
+import torchvision.models as models
+
+from tqdm import tqdm
 
 import os
 import argparse
@@ -21,6 +24,7 @@ from torch.autograd import Variable
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
 parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
 parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
+parser.add_argument('--sort', action='store_true', help='sort the dataset')
 args = parser.parse_args()
 
 use_cuda = torch.cuda.is_available()
@@ -41,11 +45,116 @@ transform_test = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=False, transform=transform_train)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
 
-testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=False, transform=transform_test)
-testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
+def flatten(x):
+    return x.view(x.numel(), -1)
+
+
+class SortingNetwork(nn.Module):
+    def __init__(self):
+        super(SortingNetwork, self).__init__()
+        self.vgg = models.vgg16(pretrained=True)
+        self.vgg.features = nn.Sequential(
+            *(self.vgg.features[i] for i in range(29)))
+
+    def forward(self, image):
+        normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                         std=[0.229, 0.224, 0.225])
+
+        normalized_image = normalize(image.data)
+
+        feature_representation = self.vgg.features(Variable(normalized_image))
+
+        return flatten(feature_representation)
+
+
+    def __str__(self):
+        return str(self.vgg.features)
+
+sorting_network = SortingNetwork()
+
+if use_cuda:
+    sorting_network.cuda()
+
+
+trainset = torchvision.datasets.CIFAR10(root='./data',
+                                        train=True,
+                                        download=True,
+                                        transform=transform_train)
+
+
+def sorting_network_prediction(tensor):
+    variable_tensor = Variable(tensor)
+
+    if use_cuda:
+        variable_tensor = variable_tensor.cuda()
+
+    tensor_prediction = sorting_network(variable_tensor)
+
+    return tensor_prediction.data
+
+
+def find_average_tensor(dataset):
+    print('Finding average tensor of dataset...')
+    temp_dataloader = torch.utils.data.DataLoader(trainset,
+                                                  batch_size=1,
+                                                  num_workers=1)
+    sum_tensor = torch.FloatTensor(torch.zeros(1, 512, 2, 2))
+
+    if use_cuda:
+        sum_tensor = sum_tensor.cuda()
+
+    dataset_size = len(temp_dataloader)
+
+    for batch_idx, (inputs, targets) in enumerate(tqdm(temp_dataloader)):
+        if use_cuda:
+            inputs = inputs.cuda()
+
+        input_prediction = sorting_network_prediction(inputs)
+
+        sum_tensor = input_prediction + flatten(sum_tensor)
+
+    average_tensor = sum_tensor / dataset_size
+
+    if use_cuda:
+        average_tensor = average_tensor.cpu()
+
+    return average_tensor
+
+
+from scipy.spatial.distance import cosine
+
+def key_function(dataset_sample, average_prediction):
+
+    prediction = sorting_network_prediction(torch.unsqueeze(dataset_sample[0], 0))
+    squeezed_prediction = torch.squeeze(prediction)
+
+    similarity = cosine(average_prediction.cpu().numpy(), squeezed_prediction.cpu().numpy())
+
+    return similarity
+
+
+if args.sort:
+    average_prediction = find_average_tensor(trainset)
+
+    trainset = sorted(trainset, key=lambda x: key_function(x, average_prediction))[::-1]
+
+# sorted(trainset, key=)
+train_batch_size = 128
+trainloader = torch.utils.data.DataLoader(trainset,
+                                          batch_size=train_batch_size,
+                                          shuffle=False,
+                                          num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(root='./data',
+                                       train=False,
+                                       download=False,
+                                       transform=transform_test)
+test_batch_size = 100
+testloader = torch.utils.data.DataLoader(testset,
+                                         batch_size=test_batch_size,
+                                         shuffle=False,
+                                         num_workers=2)
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
@@ -61,11 +170,12 @@ if args.resume:
 else:
     print('==> Building model..')
     # net = VGG('VGG19')
-    # net = ResNet18()
+    net = ResNet18()
     # net = GoogLeNet()
     # net = DenseNet121()
     # net = ResNeXt29_2x64d()
-    net = MobileNet()
+    # net = MobileNet()
+
 
 if use_cuda:
     net.cuda()
@@ -85,6 +195,7 @@ def train(epoch):
     for batch_idx, (inputs, targets) in enumerate(trainloader):
         if use_cuda:
             inputs, targets = inputs.cuda(), targets.cuda()
+
         optimizer.zero_grad()
         inputs, targets = Variable(inputs), Variable(targets)
         outputs = net(inputs)
@@ -98,7 +209,7 @@ def train(epoch):
         correct += predicted.eq(targets.data).cpu().sum()
 
         progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-            % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+                     % (train_loss/(batch_idx+1), 100. * correct/total, correct, total))
 
 def test(epoch):
     global best_acc
@@ -109,9 +220,9 @@ def test(epoch):
     for batch_idx, (inputs, targets) in enumerate(testloader):
         if use_cuda:
             inputs, targets = inputs.cuda(), targets.cuda()
-        inputs, targets = Variable(inputs, volatile=True), Variable(targets)
-        outputs = net(inputs)
-        loss = criterion(outputs, targets)
+            inputs, targets = Variable(inputs, volatile=True), Variable(targets)
+            outputs = net(inputs)
+            loss = criterion(outputs, targets)
 
         test_loss += loss.data[0]
         _, predicted = torch.max(outputs.data, 1)
@@ -119,7 +230,7 @@ def test(epoch):
         correct += predicted.eq(targets.data).cpu().sum()
 
         progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-            % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+                     % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
 
     # Save checkpoint.
     acc = 100.*correct/total
@@ -132,8 +243,8 @@ def test(epoch):
         }
         if not os.path.isdir('checkpoint'):
             os.mkdir('checkpoint')
-        torch.save(state, './checkpoint/ckpt.t7')
-        best_acc = acc
+            torch.save(state, './checkpoint/ckpt.t7')
+            best_acc = acc
 
 
 for epoch in range(start_epoch, start_epoch+200):
--- models/resnet.py
+++ models/resnet.py
@@ -148,7 +148,7 @@ class ResNet(nn.Module):
             self.in_planes = planes * block.expansion
         return nn.Sequential(*layers)
 
-    def forward(self, x):
+    def forward(self, x, return_representation=False):
         out = F.relu(self.bn1(self.conv1(x)))
         out = self.layer1(out)
         out = self.layer2(out)
@@ -156,6 +156,8 @@ class ResNet(nn.Module):
         out = self.layer4(out)
         out = F.avg_pool2d(out, 4)
         out = out.view(out.size(0), -1)
+        if return_representation:
+            return out
         out = self.linear(out)
         return out
 
